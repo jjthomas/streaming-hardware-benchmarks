c38e7d15af15aa7488ff4f66cdf59b0d609879ff
James Thomas <jamesjoethomas@gmail.com>
2017-11-09 00:08:01 -0800

    switched to 64-bit buffer

--- huffman/encode.cpp
+++ huffman/encode.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <assert.h>
 #include <sys/time.h>
@@ -14,10 +15,11 @@ using namespace std;
 int main(int argc, char **argv) {
   int NUM_BYTES = atoi(argv[1]);
   int DICT_SIZE = atoi(argv[2]);
+  assert(DICT_SIZE <= 255);
   uint8_t *input = new uint8_t[NUM_BYTES];
   uint8_t *dict = new uint8_t[DICT_SIZE];
   int *dict_lens = new int[DICT_SIZE];
-  uint8_t *output = new uint8_t[NUM_BYTES];
+  uint64_t *output = new uint64_t[NUM_BYTES / 8];
   for (int i = 0; i < NUM_BYTES; i++) {
     input[i] = (uint8_t)(rand() % DICT_SIZE);
   }
@@ -30,28 +32,28 @@ int main(int argc, char **argv) {
     dict_lens[i] = len;
   }
 
-  int num_output_bytes = 0;
-  uint8_t cur = 0;
+  int num_output_words = 0;
+  uint64_t cur = 0;
   int cur_len = 0;
   struct timeval start, end, diff;
   gettimeofday(&start, 0);
   for (int i = 0; i < NUM_BYTES; i++) {
-    uint8_t sym = dict[input[i]]; 
-    uint8_t len = dict_lens[input[i]]; 
-    cur = cur | ((sym & ((1 << (8 - cur_len)) - 1)) << cur_len);
+    uint64_t sym = dict[input[i]];
+    int len = dict_lens[input[i]];
+    cur = cur | ((sym & ((1L << (64 - cur_len)) - 1)) << cur_len);
     cur_len += len;
-    if (cur_len >= 8) {
-      output[num_output_bytes++] = cur;
-      cur_len -= 8;
+    if (cur_len >= 64) {
+      output[num_output_words++] = cur;
+      cur_len -= 64;
       cur = sym >> (len - cur_len);
     }
   }
   if (cur_len > 0) {
-    output[num_output_bytes++] = cur;
+    output[num_output_words++] = cur;
   }
   gettimeofday(&end, 0);
   timersub(&end, &start, &diff);
-  printf("%d: %ld.%06ld\n", (int)output[num_output_bytes - 1], (long)diff.tv_sec, (long)diff.tv_usec);
+  printf("%" PRIu64 ": %ld.%06ld\n", output[num_output_words - 1], (long)diff.tv_sec, (long)diff.tv_usec);
 
   return 0;
 }
